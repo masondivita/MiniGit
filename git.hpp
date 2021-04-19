fc016fc8fe145866c1a148d2729d143d098efd0e
MasonDivita <masondivita038@gmail.com>
2021-04-19 14:43:35 -0600

    Second

--- git.hpp
+++ git.hpp
@@ -2,7 +2,38 @@
 #define GIT_HPP
 
 
+struct doublyNode{
+    int commitNumber;
+    singlyNode* head;
+    doublyNode* previous;
+    doublyNode* next;
+};
 
+struct singlyNode{
+    std::string fileName; // Name of local file
+    std::string fileVersion; // Name of file in .minigit folder
+    singlyNode* next;
+};
+
+
+class miniGit
+{
+  private:
+    doublyNode* head;
+    doublyNode* tail;
+
+  public:
+    miniGit(){
+    	head = NULL;
+        tail = NULL;
+    }
+    ~miniGit();
+    void addFiles(string fileName, string fileVersion);
+    void removeFiles(string fileName, string fileVersion);
+    void commit();
+    void checkout(int version);
+    
+};
 
 
 
