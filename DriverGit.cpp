e10859c352399d7cb60b780d0077446c655c1002
ajsullivan412 <ajsullivan412@gmail.com>
2021-04-19 16:05:07 -0600

    started driver outline and add file

--- DriverGit.cpp
+++ DriverGit.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "git.hpp"
 using namespace std;
 
 int main() {
+    int option;
 
+    while(option != 5){
+
+        cout << "--Menu--" << endl;
+        cout << "1. Add file" << endl;
+        cout << "2. Remove file" << endl;
+        cout << "3. Commit" << endl;
+        cout << "4. Checkout" << endl;
+        cout << "5. Quit" << endl;
+        cin >> option;
+
+        switch(option){
+            case 1:
+                cout << "Enter the file name: " << endl;
+
+                string file;
+                getline(cin, file);
+                ifstream in_file(file);
+
+        }
+    }
 }
--- git2.cpp
+++ git2.cpp
@@ -1,4 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "git.hpp"
 
 using namespace std;
+
+bool search(string name){
+    singlyNode* ptr = head;
+    while (ptr != NULL && ptr->fileName != name){
+        ptr = ptr->next;
+        if(ptr->fileName == name){
+            return true;
+        }
+    }
+    return false;
+}
+void miniGit::addFiles(string fileName, string fileVersion){
+    cout << "Enter the file name: " << endl;
+
+    string file;
+    getline(cin, file);
+    ifstream in_file(file);
+
+    if (in_file.fail()){
+        cout << "File not found. Please enter valid filename." << endl;
+    }
+
+    if(search(fileName)){
+        cout << "File already exists in directory." << endl;
+    }
+
+}
