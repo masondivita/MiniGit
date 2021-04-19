5082253da38b63be367aa3330d77b84f0a41ad13
masondivita <82617748+masondivita@users.noreply.github.com>
2021-04-19 15:20:39 -0600

    Add files via upload

--- git.hpp
+++ git.hpp
@@ -2,7 +2,39 @@
 #define GIT_HPP
 
 
+struct doublyNode{
+    int commitNumber;
+    singlyNode* head;
+    doublyNode* previous;
+    doublyNode* next;
+};
 
+struct singlyNode{
+    std::string fileName; // Name of local file
+    std::string fileVersion; // Name of file in .minigit folder
+    singlyNode* next;
+};
+
+
+class miniGit
+{
+  private:
+    doublyNode* head;
+    doublyNode* tail;
+    singlyNode* currHead;
+
+  public:
+    miniGit(){
+    	head = NULL;
+        tail = NULL;
+    }
+    ~miniGit();
+    void addFiles(string fileName);
+    void removeFiles(string fileName);
+    void commit();
+    void checkout(int version);
+    
+};
 
 
 
--- git1.cpp
+++ git1.cpp
@@ -2,3 +2,18 @@
 #include "git.hpp"
 
 using namespace std;
+
+void miniGit::removeFiles(string fileName) {
+    singlyNode* curr = currHead;
+    singlyNode* prev = NULL;
+
+    while(curr != NULL) {
+        if (curr->fileName == fileName) {
+            prev->next = curr->next;
+            delete curr;
+            curr = 0;
+            return;
+        }
+    }
+    cout << "No file found!" << endl;
+}
