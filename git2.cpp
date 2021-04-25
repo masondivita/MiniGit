eaf29aec71e010714033547a59995b62e922a3b3
MasonDivita <masondivita038@gmail.com>
2021-04-25 03:19:17 -0600

    Working commit and I edited the checkout to at least work on my computer

--- git2.cpp
+++ git2.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 bool search(string name, singlyNode* head){
     singlyNode* ptr = head;
-    while (ptr != NULL && ptr->fileName != name){
+    while (ptr != NULL){
         ptr = ptr->next;
         if(ptr->fileName == name){
             return true;
@@ -15,26 +15,79 @@ bool search(string name, singlyNode* head){
     }
     return false;
 }
+
 void miniGit::addFiles(){
     cout << "Enter the file name: " << endl;
 
-    string file;
-    cin >> file;
-
-
-    singlyNode* nn = new singlyNode;
-    nn->fileName = file;
-    nn->fileVersion = "";
-    currHead = nn;
-
-    ifstream in_file(file);
+    string fileName;
+    string fileVersion = "";
+    cin >> fileName;
+    ifstream in_file(fileName);
 
     if (in_file.fail()){
         cout << "File not found. Please enter valid filename." << endl;
     }
 
-    if(search(file, currHead)){
-        cout << "File already exists in directory." << endl;
+    in_file.close();
+
+    // if(search(fileName, currHead)){
+    //     cout << "File already exists in directory." << endl;
+    // }
+
+     if(currHead == NULL){
+        singlyNode* nn = new singlyNode;
+        currHead = nn;
+        nn->fileName = fileName;
+        nn->next = NULL;
+        nn->fileVersion = fileVersion;
+    } else {
+        singlyNode* last = currHead;
+        while(last->next){
+            last = last->next;
+        }
+        singlyNode* nn = new singlyNode;
+        nn->fileName = fileName;
+        nn->next = NULL;
+        nn->fileVersion = fileVersion;
+        last->next = nn;
     }
-    
 }
+
+void checkoutFile(string fileName, string fileVersion){
+    ofstream currFile(fileName);
+    ifstream oldFile(fileVersion);
+    string line;
+    string oldLine;
+
+    while(!oldFile.eof()) { // copies line by line
+        oldFile >> oldLine;
+        currFile << line << endl;
+    }
+
+    oldFile.close();
+    currFile.close();
+}
+
+void miniGit::checkout(int version){
+    doublyNode *node = head;
+    cout << "Warning: Files in current directtory will be overwritten" << endl;
+    cout << "Enter commit number: " << endl;
+    cin >> version;
+    if(head == NULL){
+        cout << "No previous file version availible." << endl;
+        return;
+    }
+    while(node){
+        node = node->next;
+        if(node->commitNumber == version){
+            singlyNode *curr = currHead;
+            while(curr){
+                checkoutFile(curr->fileName, curr->fileVersion);
+                curr = curr->next;
+            }
+        }
+    }
+}
+
+
+
